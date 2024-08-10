7909cbac5bcaab2c11dfc3fa1eab7989b62c0d6f
Eosoft89 <ericrojas.ing@gmail.com>
2024-08-10 13:37:40 -0400

    Update

--- obj/Debug/net7.0/FruitAPI.AssemblyInfo.cs
+++ obj/Debug/net7.0/FruitAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FruitAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6db8dd361ddb86f7e0b54ba73acdad19850b34b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FruitAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FruitAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
